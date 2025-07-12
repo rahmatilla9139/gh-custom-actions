25f2bdf26a30ae10c3638015d1371bf9ec95fec2
rahmatilla9139 <architect9139@gmail.com>
2025-07-12 17:57:13 +0500

    fixed shell args

--- .github/actions/deploy-s3-docker/entrypoint.sh
+++ .github/actions/deploy-s3-docker/entrypoint.sh
@@ -7,7 +7,7 @@ BUILD_FOLDER=${INPUT_DIST_FOLDER}
 aws configure list
 
 S3URI="s3://${BUCKET}"
-aws s3 sync ${BUILD_FOLDER} ${S3URI} --region ${BUCKET_REGION}
+aws s3 sync ${BUILD_FOLDER} ${S3URI} --region "${BUCKET_REGION}"
 URL="http://${BUCKET}.s3-website-${BUCKET_REGION}.amazonaws.com"
 
 echo "website-url=${URL}"
